75821f985e2c0c217b13416143949f398e705162
ducanh-99 <ducanhhust99@gmail.com>
2021-09-22 17:41:07 +0700

    add base email sender

--- services/hr/app/api/v1/api_payment.py
+++ services/hr/app/api/v1/api_payment.py
@@ -1,9 +1,10 @@
+from fastapi_simple_security import api_key_security
 from app.schemas.sche_payment import PaymentRequest
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 router = APIRouter()
 
-@router.post("/payment")
+@router.post("/payment", dependencies=[Depends(api_key_security)])
 def payment(payment_request: PaymentRequest):
     pass
 
